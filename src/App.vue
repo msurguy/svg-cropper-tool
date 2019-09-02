dc982cef07831697a681479e2512122597749323
Maks Surguy <msurguy@gmail.com>
2019-09-01 18:27:44 -0700

    use SVGO

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,26 @@
       <div class="controls-wrapper">
         <div class="controls">
           <p class="mt-3 lead text-center text-white"> Source </p>
-          <button class="btn btn-secondary btn-block">Upload SVG</button>
-          <toggle label="Closed Path" v-model="source.closedPath"></toggle>
+          <div class="sidebar-control">
+            <div class="control-header">
+              <div></div>
+              <div class="control-label">
+                Upload File
+              </div>
+              <div></div>
+            </div>
+            <div class="custom-file">
+              <input @change="onFileChange" type="file" accept="image/svg+xml" class="custom-file-input"
+                     id="sourcefile">
+              <label class="custom-file-label" for="sourcefile">Choose file</label>
+            </div>
+          </div>
 
+          <toggle label="Closed Path" v-model="source.closedPath"></toggle>
           <p class="mt-3 lead text-center text-white"> Cropper  </p>
-          <button class="btn btn-secondary btn-block">Upload SVG</button>
+          <select-field label="Cropper type" v-model="cropper.type"
+                        :options="cropper.options"></select-field>
+
           <toggle label="Closed Path" v-model="cropper.closedPath"></toggle>
         </div>
       </div>
@@ -25,7 +40,7 @@
     <!-- Page Content -->
     <div class="paper">
       <div class="sketch">
-
+        <img :src="source.svg" alt="">
       </div>
     </div>
     <div class="footer-wrapper">
@@ -40,25 +55,62 @@
 </template>
 
 <script>
+  /* eslint-disable no-console */
+  import bsCustomFileInput from 'bs-custom-file-input'
+
   import {eventBus} from '@/main'
   import Toggle from "@/components/Toggle";
+  import SelectField from "@/components/SelectField";
+  import actions from '@/svgo'
+
   export default {
     name: 'App',
     components: {
-      Toggle
+      Toggle,
+      SelectField
     },
-    data() {
+    mounted () {
+      bsCustomFileInput.init()
+    },
+    data () {
       return {
         source: {
           svg: null,
+          optimized: null,
           closedPath: false,
           position: [0, 0]
         },
         cropper: {
-          svg: null,
+          position: [0, 0],
           closedPath: false,
-          currentShape: 'square',
-          position: [0, 0]
+          type: 'square',
+          options: [
+            {text: 'Square', value: 'square'},
+            {text: 'Rectangle', value: 'rectangle'},
+            {text: 'Circle', value: 'circle'},
+            {text: 'Polygon', value: 'polygon'},
+            {text: 'Custom Shape', value: 'custom'},
+            {text: 'SVG', value: 'svg'}
+          ],
+          square: {
+            size: [200, 200],
+          },
+          rectangle: {
+            size: [100, 200],
+          },
+          circle: {
+            size: [200],
+          },
+          polygon: {
+            size: [200],
+            sides: 5,
+          },
+          custom: {
+            points: []
+          },
+          svg: {
+            file: null
+          }
         },
         cropperOptions: {
           square: [0,0],
@@ -69,7 +121,38 @@
     methods: {
       download() {
         eventBus.$emit('download')
-      }
+      },
+      onFileChange(e) {
+        let files = e.target.files || e.dataTransfer.files;
+        if (!files.length)
+          return;
+        this.createImage(files[0]);
+      },
+      async multipass (svg) {
+        await actions.load({data: svg})
+        let fileResult = await actions.process()
+        const iterationCallback = async function(){}
+
+        let resultFile = {text: fileResult.data, width: fileResult.dimensions.width, height: fileResult.dimensions.height}
+        // eslint-disable-next-line no-cond-assign
+        while (resultFile = await actions.nextPass()) {
+          await iterationCallback(resultFile);
+        }
+        // let result = await actions.nextPass()
+        // result = await actions.nextPass()
+        // result = await actions.nextPass()
+        console.log(resultFile)
+      },
+      createImage(file) {
+        const reader = new FileReader();
+        const vm = this;
+
+        reader.onload = (e) => {
+          vm.source.svg = "data:image/svg+xml;charset=utf-8," + encodeURIComponent(e.target.result)
+          this.multipass(e.target.result)
+        };
+        reader.readAsText(file);
+      },
     }
   }
 </script>
@@ -105,12 +188,17 @@
   }
 
   .paper {
+    padding: 10px;
     background-color: #dedede;
     position: relative;
     max-height: 100vh;
     width: calc(100% - 300px);
     overflow: scroll;
     z-index: 1;
+  }
+
+  .sketch img{
+    border: 1px dashed #000;
   }
 
   .sidebar {
